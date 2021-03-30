b190a894413efeede2c34283d31e6fc8a154d015
Jonathan Heugh <mrjbheugh@gmail.com>
2021-03-30 16:41:52 +0200

    Completed level 100

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ https://github.com/CodeYourFuture/syllabus/tree/master/js-core-3/tv-show-dom-pro
 The solution to this project will be given to you after you have completed it. You will be able to find the solition here
 
 https://github.com/CodeYourFuture/tv-show-dom-project-solution
+
+Level 100
+1. All episodes must be shown
+
+2. For each episode, AT LEAST following must be displayed:
+    i) the episode's name
+    ii) the season number
+    iii) the episode number
+    iv) the episode's medium-sized image
+    v) the episode's summary text
+
+3. You should combine season number and episode number into an episode code:
+    i) Each part should be zero-padded to two digits.
+    ii) Example: S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
+
+4. Your page should state somewhere that the data has (originally) come from TVMaze.com, and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
--- script.js
+++ script.js
@@ -4,9 +4,30 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function zero(num) {
+  return num < 10 ? "0" + num : num;
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((item) => {
+    let episodeDiv = document.createElement("div");
+    episodeDiv.setAttribute("class", "episode");
+    rootElem.appendChild(episodeDiv);
+    let pTag = document.createElement("p");
+    episodeDiv.appendChild(pTag);
+    pTag.setAttribute('class', 'title');
+    pTag.innerHTML = `<strong>${item.name} - S${zero(item.season)}E${zero(
+      item.number
+    )}</strong> `;
+    let imgTag = document.createElement("img");
+    episodeDiv.appendChild(imgTag);
+    imgTag.src = `${item.url}`;
+    let pSummary = document.createElement("p");
+    episodeDiv.appendChild(pSummary);
+    pSummary.innerHTML = `${item.summary}`;
+  });
 }
 
 window.onload = setup;
